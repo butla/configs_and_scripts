61402398efc48f79315cdd283d581be93d8d20d3
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2020-08-05 19:03:59 +0200

    Random password creation shell function.

--- home/.config/zsh/functions.zsh
+++ home/.config/zsh/functions.zsh
@@ -86,3 +86,9 @@ function shrug()
     echo "$_SHRUG copied to clipboard..."
     echo $_SHRUG | xclip -selection clipboard
 }
+
+function passwordgen()
+{
+    python3 -c "import secrets; print(secrets.token_urlsafe(16), end='')" | xclip -selection clipboard
+    echo "Password created and copied to clipboard..."
+}
