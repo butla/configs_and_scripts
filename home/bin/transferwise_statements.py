a5600d09515a0dd1fdf55ccec2f8b4bf4b5a03fa
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2020-08-08 12:30:30 +0200

    Idea for the statement script.

---
+++ home/bin/transferwise_statements.py
@@ -0,0 +1,5 @@
+
+# TODO get gbp and USD for previous month (if it's before 10) or current (if after 10)
+# take month as param
+# name them, put in dropbox folder (separete function for that)
+# tests
