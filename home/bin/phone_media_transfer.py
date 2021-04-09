59a9f1bea5a7a001ebe08853d6d0acbbb2f94906
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2021-04-09 19:08:02 +0200

    Phone photo transfer using rsync.

--- home/bin/phone_media_transfer.py
+++ home/bin/phone_media_transfer.py
@@ -4,21 +4,16 @@
 A script to run on your phone (running in Termux under Android).
 """
 
-# TODO prepare a list of files for rsync
-# - save that list with date
-
+# TODO local sync with SFTP to an isolated location on the computer? Some watcher would pick up the files.
 # TODO sync to cloud
-# - get that filename from scaleaway
+# - get last filename from scaleaway
 # - send encrypted chunks (contain entire photos)
 # - encrypted manifest of files
 # - can we use minio or nextcloud? Then encryption wouldn't be necessary.
 #   - would be nice if VM drive would be encrypted
-
-# TODO up
-# TODO zip all the audio/video from /storage/9CXXXXXXX/Camera
-# the output file needs to be in memory
-# TODO send file to scaleaway?
-# TODO check file hash to make sure it's not broken
+# TODO make it possible to specify the device by the hostname. Look it up first? How?
+# TODO Getting the script args with Typer would be better, but I'd need to package it with the dependencies.
+# Or vendor typer into this repo.
 
 import argparse
 import shlex
@@ -26,11 +21,12 @@ import subprocess
 import sys
 from datetime import datetime
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 from zipfile import ZipFile
 
-MEDIA_DIR=Path('/storage/0123-4567/DCIM/Camera/')
-PC_PHOTOS_DIR='/data/zdjęcia_i_filmiki/z_telefonu_mego/samsung_s7'
+MEDIA_DIR=Path('/storage/9C33-6BBD/DCIM/Camera/')
+# TODO is this the right path?
+PC_PHOTOS_DIR='/data/zdjęcia_i_filmiki/z_telefonu_mego/samsung_s10'
 
 
 def _get_latest_photo_on_pc(pc_ip: str) -> str:
@@ -39,46 +35,33 @@ def _get_latest_photo_on_pc(pc_ip: str) -> str:
     return Path(photo_path).name
 
 
-def _zip_latest_photos(older_than_file_name: str) -> str:
-    new_photos = [path for path in MEDIA_DIR.iterdir()
-                  if path.name > older_than_file_name]
-    start_file_name = older_than_file_name.replace('.', '_')
-    zip_name = f'/sdcard/Download/photos_from_{start_file_name}.zip'
+def _transfer_photos(files_to_send: List[Path], pc_ip: str, target_path: str = PC_PHOTOS_DIR):
+    # Prepending ./ so that rsync knows this is a local file path.
+    # TODO make this timezone aware
+    media_list_filename = f'./photos_to_transfer_on_{datetime.now().isoformat()}.txt'
+    with open(media_list_filename, 'w') as media_list_file:
+        for path in files_to_send:
+            # Taking only the name of the file, so that all paths are relative to the media directory,
+            # so that rsync won't put the files in any subdirectories.
+            # There shouldn't be any subdirectories there.
+            media_list_file.write(path.name + '\n')
 
-    print(f'Zipping {len(new_photos)} photos from {str(MEDIA_DIR)} to {zip_name}')
-    with ZipFile(zip_name, mode='w') as zip_file:
-        for path in new_photos:
-            zip_file.write(
-                str(path),
-                arcname=path.name,
-            )
-
-    return zip_name
-
-
-def _transfer_photos(zip_path:str, pc_ip: str):
-    print("Transferring photos' zip")
+    print(f'Transferring the files listed in {media_list_filename} with rsync...')
     subprocess.run(
-        shlex.split(f'scp {zip_path} butla@{pc_ip}:{PC_PHOTOS_DIR}'),
+        [
+            'rsync', '--update', '--no-owner', '--progress',
+            '--files-from', media_list_filename,
+            str(MEDIA_DIR), f'butla@{pc_ip}:{target_path}'
+        ],
         check=True,
     )
-
-    print("Unpacking the photos")
-    zip_file_name = Path(zip_path).name
-    remote_commands = f'cd {PC_PHOTOS_DIR}; unzip {zip_file_name}; rm {zip_file_name}'
-    subprocess.run(
-        shlex.split(f'ssh butla@{pc_ip} "{remote_commands}"'),
-        check=True,
-    )
-
-    print("All done")
 
 
 def _get_files_to_send(
         media_folder: Path,
         older_than_file_name: str,
         up_to_file: Optional[str] = None,
-):
+) -> List[Path]:
     """
     Args:
         media_folder: folder we'll take the media files from
@@ -94,8 +77,6 @@ def _get_files_to_send(
     return list(files_without_upper_boundary)
 
 
-# TODO Would be better with typer, but I'd need to package it with the dependencies.
-# Or vendor typer into this repo.
 def _parse_program_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Transfers media files to another machine.')
     parser.add_argument('pc_ip', type=str,
@@ -108,7 +89,6 @@ def _parse_program_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-# TODO make it possible to specify the device by the hostname. Look it up first? How?
 def send_over_wlan(
         pc_ip: str,
         last_synced_file: Optional[str] = None,
@@ -127,32 +107,9 @@ def send_over_wlan(
         older_than_file_name=last_synced_file,
         up_to_file=up_to_file,
     )
+    _transfer_photos(files_to_send, pc_ip)
 
-    # TODO make this timezone aware
-    media_list_filename = f'photos_to_transfer_on_{datetime.now().isoformat()}.txt'
-    with open(media_list_filename, 'w') as media_list_file:
-        media_list_file.writelines(str(path) for path in files_to_send)
-
-    print(f'Transferring the files listed in {media_list_filename} with rsync...')
-    subprocess.run(
-        [
-            'rsync', '--update', '--no-owner', '-v'
-            f'--files-from={media_list_filename}',
-            MEDIA_DIR, 'butla@{pc_ip}:{PC_PHOTOS_DIR}'
-        ],
-        check=True,
-    )
     print('Success!')
-    # TODO get the files, save to a file, use it in files from
-    # rsync --archive --update --no-owner --files-from=rsync_list.txt --dry-run -vv rsync_source/ rsync_target/
-
-    # zip_path = _zip_latest_photos(last_synced_photo)
-
-    # print('Sha1 sum of the photos archive:')
-    # subprocess.run(['sha1sum', zip_path], check=True)
-    # input('Press any key to delete the photos archive file...')
-    # os.unlink(zip_path)
-    # print('Archive removed.')
 
 
 if __name__ == '__main__':
@@ -162,10 +119,3 @@ if __name__ == '__main__':
         last_synced_file=arg_parser.files_after,
         up_to_file=arg_parser.up_to_file,
     )
-
-    # TODO this doesn't work, because Android is a little shit that will kill
-    # long network connections no matter what I'm telling it
-    # transfer_photos(zip_path, pc_ip)
-    #
-    # print('Removing the zip file at', zip_path)
-    # os.remove(zip_path)
